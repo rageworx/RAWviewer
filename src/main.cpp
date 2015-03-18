6706dce478dc3cd2c06ff8391e2bddf8920a592f
Raphael Kim <rageworx@gmail.com>
2015-03-18 11:30:47 +0900

    Updaing for add sources.
    Added some sources for embodying viewer.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include "winMain.h"
 
+#define START_WIN_WIDTH     600
+#define START_WIN_HEIGHT    400
+
 static winMain* wMain = NULL;
 
 int main (int argc, char ** argv)
 {
-    wMain = new winMain( 600, 400, argc, argv );
+    wMain = new winMain( START_WIN_WIDTH, START_WIN_HEIGHT, argc, argv );
 
     if ( wMain != NULL )
     {
